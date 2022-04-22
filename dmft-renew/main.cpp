3df99e3e2f2df87c32b78e9443e14720c2cff7d1
aPLAsoldier <yshilalala@gmail.com>
2022-04-22 12:59:39 -0400

    Initial Commit

---
+++ dmft-renew/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dmft-renew
+//
+//  Created by Yin Shi on 4/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
