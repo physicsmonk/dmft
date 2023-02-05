dad4a952d3d6af7c4d1a75b9d250b9bde8902aeb
aPLAsoldier <yshilalala@gmail.com>
2023-02-05 01:24:08 -0500

    Start ignoring Build/

--- Build/Intermediates.noindex/dmft.build/Release/dmft.build/Objects-normal/arm64/pugixml.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/yinshi/Documents/dmft/dmft/dependencies/pugixml/pugixml.cpp \
-  /Users/yinshi/Documents/dmft/dmft/dependencies/pugixml/pugixml.hpp \
-  /Users/yinshi/Documents/dmft/dmft/dependencies/pugixml/pugiconfig.hpp
