83fcfcedb6345a062fce3f0c0a2fcfc967b72af8
aPLAsoldier <yshilalala@gmail.com>
2023-02-04 21:26:30 -0500

    Switched to not adjusting Pulay mixing parameter in MQEM
    (cherry picked from commit 306fe1b342d0b3b9f7cfb51f9a0289dc2f6fc837)

---
+++ Build/Intermediates.noindex/dmft.build/Release/dmft.build/Objects-normal/arm64/pugixml.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/yinshi/Documents/dmft/dmft/dependencies/pugixml/pugixml.cpp \
+  /Users/yinshi/Documents/dmft/dmft/dependencies/pugixml/pugixml.hpp \
+  /Users/yinshi/Documents/dmft/dmft/dependencies/pugixml/pugiconfig.hpp
